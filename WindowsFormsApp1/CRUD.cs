8961aaca5401a3ab1fe9eca7eff03ab1f41f3931
AAFAQUE AHMAD <58232054+AAA1999mzp@users.noreply.github.com>
2024-10-15 18:36:34 +0530

    file

---
+++ WindowsFormsApp1/CRUD.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class CRUD : Form
+    {
+        public CRUD()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=LAPTOP-IIT55QJI;Initial Catalog=CRUDForm;Integrated Security=True;TrustServerCertificate=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("insert into ut values(@id,@name,@age)", con);
+            cmd.Parameters.AddWithValue("@id", int.Parse(textBox1.Text));
+            cmd.Parameters.AddWithValue("@name", textBox2.Text);
+            cmd.Parameters.AddWithValue("@age", double.Parse(textBox3.Text));
+            cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Saved Succesfully");
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=LAPTOP-IIT55QJI;Initial Catalog=CRUDForm;Integrated Security=True;TrustServerCertificate=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("update ut set name=@name, age=@age where id=@id", con);
+            cmd.Parameters.AddWithValue("@id", int.Parse(textBox1.Text));
+            cmd.Parameters.AddWithValue("@name", textBox2.Text);
+            cmd.Parameters.AddWithValue("@age", double.Parse(textBox3.Text));
+            cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Updated Succesfully");
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=LAPTOP-IIT55QJI;Initial Catalog=CRUDForm;Integrated Security=True;TrustServerCertificate=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("delete ut where id=@id", con);
+            cmd.Parameters.AddWithValue("@id", int.Parse(textBox1.Text));
+            cmd.Parameters.AddWithValue("@name", textBox2.Text);
+            cmd.Parameters.AddWithValue("@age", double.Parse(textBox3.Text));
+            cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Deleted Succesfully");
+        }
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=LAPTOP-IIT55QJI;Initial Catalog=CRUDForm;Integrated Security=True;TrustServerCertificate=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select * from ut ", con);
+            //cmd.Parameters.AddWithValue("@id", int.Parse(textBox1.Text));
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
